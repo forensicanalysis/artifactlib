c6bf00e2769552900f17a56e7525a449ff69d387
Jonas Plum <git@cugu.eu>
2020-01-15 23:13:46 +0100

    Set base url

--- pyartifacts/docs/src/conf.py
+++ pyartifacts/docs/src/conf.py
@@ -24,6 +24,8 @@ author = 'Jonas Plum'
 # The full version, including alpha/beta/rc tags
 release = '0.1.0'
 
+html_baseurl = 'https://forensicanalysis.github.io/artifactlib/'
+
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
